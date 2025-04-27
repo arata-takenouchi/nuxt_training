aee955bca4c375984b7d655edb15e7273a0ca6bb
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-04-27 20:49:32 +0900

    auto import 4

--- app.vue
+++ app.vue
@@ -2,9 +2,17 @@
   <div>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
+    {{ double }}
   </div>
 </template>
 
 <script setup lang="ts">
-const composable =useMyComposable()
+import { ref, computed } from "#imports"
+
+// const composable =useMyComposable()
+const count = ref(1)
+const double = computed(() => count.value * 2)
+watch(count, (newVal) => {
+  console.log(newVal)
+})
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  imports: {
+    // autoImport: false // defaultでtrue、falseにするとauto import が全て無効になる
+    scan: false // こっちをfalseにすると、composablesなどのカスタム前提の機能は自動importされなくなる
+  }
 })
