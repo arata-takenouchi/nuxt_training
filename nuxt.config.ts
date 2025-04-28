61fd681189055f554cd470594c4ea66e1da63e5b
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-04-28 22:10:34 +0900

    routing 1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  components: {
-    dirs: []
-  }
+  // components: {
+  //   dirs: []
+  // }
   // imports: {
   //   autoImport: false // defaultでtrue、falseにするとauto import が全て無効になる
   //   scan: false // こっちをfalseにすると、composablesなどのカスタム前提の機能は自動importされなくなる
