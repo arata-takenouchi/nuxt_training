22e6264a402532864bc42052028791901f94f91a
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-05-01 22:20:26 +0900

    route validation

---
+++ pages/posts/[id].vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+// ページ遷移時にvalidationを行うことができる、falseの場合は404ページににリダイレクト
+definePageMeta({
+  validate: async (route) => {
+    return typeof route.params.id === 'string' && /^\d+$/.test(route.params.id)
+  }
+})
+</script>
+
+<template>
+  
+</template>
