0f25979b6171548338995cd1a30701075477bb04
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-05-03 23:42:49 +0900

    more page meta

---
+++ pages/some-page.vue
@@ -0,0 +1,24 @@
+<script setup>
+definePageMeta({
+  // [id]-[slug].vueみたいなpathがあるとき、/test-categoryみたいなパスは該当しないよう、正規表現でチェックできる
+  path: '/:postId(\\d+)-:postSlug'
+
+  // layouts/ディレクトリの中で一致するファイルを指定
+  layout: 'admin'
+
+  // レイアウトを無効にすることもできる
+  layout: false
+
+  key: (route) => route.fullPath,
+
+  keepalive: {
+    exclude: ['modal']
+  },
+
+  pageType: 'Checkout'
+})
+</script>
+
+<template>
+  
+</template>
