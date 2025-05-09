cbeb016f2fabc8d600b3455333e3fee2b586610e
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-05-09 23:53:02 +0900

    state 1

---
+++ components/useState.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    counter: {{ counter }}
+    <button @click="counter++">+</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+// useStateで状態を保持することも一応できる（実際はstore使うことが多いとは思う）
+const counter = useState('counter', () => Math.round(Math.random() * 1000))
+</script>
