93cd9f576bda1015a1829b129d45f422dcd82852
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-05-11 22:05:16 +0900

    state 2

---
+++ components/Locale.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { useLocale, useLocales, useLocaleDate } from '@/composables/locale';
+
+const locale = useLocale()
+const locales = useLocales()
+const date = useLocaleDate(new Date('2025-01-01'))
+</script>
+
+<template>
+  <div>
+    <h1>This Year First Date</h1>
+    <p>{{ date }}</p>
+    <label for="locale-chooser">Preview different locale</label>
+    <select id="locale-chooser" v-model="locale">
+      <option v-for="locale of locales" :key="locale" :value="locale">
+        {{ locale }}
+      </option>
+    </select>
+  </div>
+</template>
