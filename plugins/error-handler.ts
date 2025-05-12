23418b8638b017aa8c81c28d7c590d714754f3d7
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-05-12 23:08:12 +0900

    error handler 1

---
+++ plugins/error-handler.ts
@@ -0,0 +1,9 @@
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.config.errorHandler = (error, instance, info) => {
+    // こう書くとvueの全てのエラーを受け取れる（catchされた場合でも受け取る）
+  }
+
+  nuxtApp.hook('vue:error', (error, instance, info) => {
+    // こちらの場合はcatchされずにtopレベルでエラー発生した場合にtrigeerされる
+  })
+})
